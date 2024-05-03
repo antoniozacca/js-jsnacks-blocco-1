2d0ba5c830c31211320c047fd61c520abb35e2b6
antoniozacca <antoniozaccaria057@gmail.com>
2024-05-03 12:16:44 +0200

    fix

--- JSnack2/main.js
+++ JSnack2/main.js
@@ -4,11 +4,13 @@ const primaParola = prompt ("Inserisci la prima parola");
 //inserisci la seconda parola
 const secondaParola = prompt ("Inserisci la seconda parola");
 
-//controllo la lunghezza delle due parole per stampare prima la più lunga
+//controllo la lunghezza delle due parole per stampare prima la più corta
 if (primaParola.length > secondaParola.length) {
-    console.log(primaParola);
-    console.log(secondaParola);
-} else {
     console.log(secondaParola);
     console.log(primaParola);
+} else if (primaParola.length < secondaParola.length) {
+    console.log(primaParola);
+    console.log(secondaParola);
+} else if (primaParola.length === secondaParola.length){
+    console.log("le due parole hanno la stessa lunghezza");
 }
